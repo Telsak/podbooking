2631f0a6abb5c4ccbcb6027d23a3db8beb36b955
Simon Olofsson <telsak@gmail.com>
2025-12-21 01:16:31 +0100

    Fixed profile refresh

--- getapod.py
+++ getapod.py
@@ -1457,6 +1457,10 @@ def user(username, option=''):
 
         else:
             flash("Invalid update request!", "error")
+        
+        if 'userdetails' in globals() and userdetails:
+            userdetails.pop(username, None)
+        
         return redirect(url_for('user', username=username).rstrip('/'))
 
 @app.template_filter('mobile_table')
